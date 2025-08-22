dc0d8de08efbc00de05a5eb486d410e16a526d17
Andersonarjzx <andersonarjzx@gmail.com>
2025-08-22 08:47:25 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a.nav-links a, a.hero .button').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
